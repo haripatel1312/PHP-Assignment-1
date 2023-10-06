95515dd114c13e2ebd7076a6b3f8136045c9ebb0
haripatel1312 <137130311+haripatel1312@users.noreply.github.com>
2023-10-06 02:54:51 -0400

    Update process_order.php

--- process_order.php
+++ process_order.php
@@ -1,5 +1,5 @@
 <?php
-
+//connect with the server
 $con =  mysqli_connect('172.31.22.43','Hari200552675','5QhWgIHfPN','Hari200552675');
 $fname = $_POST['fname'];
 $lname = $_POST['lname'];
@@ -10,8 +10,9 @@ if(!$con){
     die("Connection Error".mysqli_connect_error());
 }
 else{
-    $toppings=isset($_POST["toppings"]) ? $_POST["toppings"] : array();
+    $toppings=isset($_POST["toppings"]) ? $_POST["toppings"] : array(); // array is used to store selected toppings checkbox
     $toppings_string=implode(", ",$toppings);
+    // implode function helps to store array values in single string
     $sql = "INSERT INTO pizza_order (first_name,last_name,contact,quantity,pizza_size,toppings) VALUES ('$fname','$lname','$contact','$quantity','$size','$toppings_string')";
     $result = mysqli_query($con,$sql);
 
